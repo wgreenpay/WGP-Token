b2faddde736da290f8b43f13bb600c962de2e46e
tien-novum <42458372+tien-novum@users.noreply.github.com>
2019-03-12 17:46:59 +0700

    New WGP tokens

--- WpayContract.sol
+++ WpayContract.sol
@@ -1,4 +1,4 @@
-pragma solidity 0.4.25;
+pragma solidity 0.5.4;
 
 /**
  * @title SafeMath
@@ -10,25 +10,23 @@ library SafeMath {
       return 0;
     }
     uint256 c = a * b;
-    assert(c / a == b);
+    require(c / a == b);
     return c;
   }
 
   function div(uint256 a, uint256 b) internal pure returns (uint256) {
-    // assert(b > 0); // Solidity automatically throws when dividing by 0
     uint256 c = a / b;
-    // assert(a == b * c + a % b); // There is no case in which this doesn't hold
     return c;
   }
 
   function sub(uint256 a, uint256 b) internal pure returns (uint256) {
-    assert(b <= a);
+    require(b <= a);
     return a - b;
   }
 
   function add(uint256 a, uint256 b) internal pure returns (uint256) {
     uint256 c = a + b;
-    assert(c >= a);
+    require(c >= a);
     return c;
   }
 }
@@ -47,103 +45,158 @@ contract ERC20 {
 contract WGP is ERC20 { 
     using SafeMath for uint256;
     //--- Token configurations ----// 
-    string public constant name = "W GREEN PAY";
-    string public constant symbol = "WGP";
-    uint8 public constant decimals = 18;
-    uint public maxCap = 1000000000 ether;
+    string private constant _name = "W GREEN PAY";
+    string private constant _symbol = "WGP";
+    uint8 private constant _decimals = 18;
+    uint256 private constant _maxCap = 600000000 ether;
+    uint256 private _icoStartDate = 1538366400;   // 01-10-2018 12:00 GMT+8
+    uint256 private _icoEndDate = 1539489600;     // 14-10-2018 12:00 GMT+8
     
     //--- Token allocations -------//
-    uint256 public _totalsupply;
-    uint256 public mintedTokens;
+    uint256 private _totalsupply;
 
     //--- Address -----------------//
-    address public owner; //Management
-    address public ethFundMain;
+    address private _owner;
+    address payable private _ethFundMain;
    
-    //--- Milestones --------------//
-    uint256 public icoStartDate = 1538366400; // 01-10-2018 12:00 pm
-    uint256 public icoEndDate = 1539489600; // 14-10-2018 12:00 pm
-    
     //--- Variables ---------------//
-    bool public lockstatus = true;
-    bool public stopped = false;
+    bool private _lockToken = false;
+    bool private _allowICO = true;
     
-    mapping(address => uint256) public balances;
-    mapping(address => mapping(address => uint256)) public allowed;
+    mapping(address => uint256) private balances;
+    mapping(address => mapping(address => uint256)) private allowed;
+    mapping(address => bool) private locked;
+    
     event Mint(address indexed from, address indexed to, uint256 amount);
     event Burn(address indexed from, uint256 amount);
+    event ChangeReceiveWallet(address indexed newAddress);
+    event ChangeOwnerShip(address indexed newOwner);
+    event ChangeLockStatusFrom(address indexed investor, bool locked);
+    event ChangeTokenLockStatus(bool locked);
+    event ChangeAllowICOStatus(bool allow);
     
     modifier onlyOwner() {
-        require (msg.sender == owner);
+        require(msg.sender == _owner, "Only owner is allowed");
         _;
     }
 
     modifier onlyICO() {
-        require(now >= icoStartDate && now < icoEndDate);
+        require(now >= _icoStartDate && now < _icoEndDate, "CrowdSale is not running");
         _;
     }
 
     modifier onlyFinishedICO() {
-        require(now >= icoEndDate);
+        require(now >= _icoEndDate, "CrowdSale is running");
         _;
     }
     
-    constructor() public
-    {
-        owner = msg.sender;
-        ethFundMain = 0x67fd4721d490A5E609cF8e09FCE0a217b91F1546;
-    }
-
-    function () public payable onlyICO {
-        
-    }
-
-    function totalSupply() public view returns (uint256 total_Supply) {
-        total_Supply = _totalsupply;
+    modifier onlyAllowICO() {
+        require(_allowICO, "ICO stopped");
+        _;
     }
     
-    function balanceOf(address _owner)public view returns (uint256 balance) {
-        return balances[_owner];
+    modifier onlyUnlockToken() {
+        require(!_lockToken, "Token locked");
+        _;
     }
 
-    function transferFrom( address _from, address _to, uint256 _amount ) public onlyFinishedICO returns (bool success)  {
-        require( _to != 0x0);
-        require(!lockstatus);
-        require(balances[_from] >= _amount && allowed[_from][msg.sender] >= _amount && _amount >= 0);
+    constructor() public
+    {
+        _owner = msg.sender;
+    }
+    
+    function name() public pure returns (string memory) {
+        return _name;
+    }
+    
+    function symbol() public pure returns (string memory) {
+        return _symbol;
+    }
+    
+    function decimals() public pure returns (uint8) {
+        return _decimals;
+    }
+    
+    function maxCap() public pure returns (uint256) {
+        return _maxCap;
+    }
+    
+    function owner() public view returns (address) {
+        return _owner;
+    }
+    
+    function ethFundMain() public view returns (address) {
+        return _ethFundMain;
+    }
+    
+    function icoStartDate() public view returns (uint256) {
+        return _icoStartDate;
+    }
+    
+    function icoEndDate() public view returns (uint256) {
+        return _icoEndDate;
+    }
+    
+    function lockToken() public view returns (bool) {
+        return _lockToken;
+    }
+    
+    function allowICO() public view returns (bool) {
+        return _allowICO;
+    }
+    
+    function lockStatusOf(address investor) public view returns (bool) {
+        return locked[investor];
+    }
+
+    function totalSupply() public view returns (uint256) {
+        return _totalsupply;
+    }
+    
+    function balanceOf(address investor) public view returns (uint256) {
+        return balances[investor];
+    }
+    
+    function approve(address _spender, uint256 _amount) public onlyFinishedICO onlyUnlockToken returns (bool)  {
+        require( _spender != address(0), "Address can not be 0x0");
+        require(balances[msg.sender] >= _amount, "Balance does not have enough tokens");
+        require(!locked[msg.sender], "Sender address is locked");
+        require(!locked[_spender], "Spender address is locked");
+        allowed[msg.sender][_spender] = _amount;
+        emit Approval(msg.sender, _spender, _amount);
+        return true;
+    }
+  
+    function allowance(address _from, address _spender) public view returns (uint256) {
+        return allowed[_from][_spender];
+    }
+
+    function transfer(address _to, uint256 _amount) public onlyFinishedICO onlyUnlockToken returns (bool) {
+        require( _to != address(0), "Receiver can not be 0x0");
+        require(balances[msg.sender] >= _amount, "Balance does not have enough tokens");
+        require(!locked[msg.sender], "Sender address is locked");
+        require(!locked[_to], "Receiver address is locked");
+        balances[msg.sender] = (balances[msg.sender]).sub(_amount);
+        balances[_to] = (balances[_to]).add(_amount);
+        emit Transfer(msg.sender, _to, _amount);
+        return true;
+    }
+    
+    function transferFrom( address _from, address _to, uint256 _amount ) public onlyFinishedICO onlyUnlockToken returns (bool)  {
+        require( _to != address(0), "Receiver can not be 0x0");
+        require(balances[_from] >= _amount, "Source's balance is not enough");
+        require(allowed[_from][msg.sender] >= _amount, "Allowance is not enough");
+        require(!locked[_from], "From address is locked");
+        require(!locked[_to], "Receiver address is locked");
         balances[_from] = (balances[_from]).sub(_amount);
         allowed[_from][msg.sender] = (allowed[_from][msg.sender]).sub(_amount);
         balances[_to] = (balances[_to]).add(_amount);
         emit Transfer(_from, _to, _amount);
         return true;
     }
-    
-    function approve(address _spender, uint256 _amount)public onlyFinishedICO returns (bool success)  {
-        require(!lockstatus);
-        require( _spender != 0x0);
-        require(balances[msg.sender] >= _amount);
-        allowed[msg.sender][_spender] = _amount;
-        emit Approval(msg.sender, _spender, _amount);
-        return true;
-    }
-  
-    function allowance(address _owner, address _spender)public view returns (uint256 remaining) {
-        require( _owner != 0x0 && _spender !=0x0);
-        return allowed[_owner][_spender];
-    }
 
-    function transfer(address _to, uint256 _amount)public onlyFinishedICO returns (bool success) {
-        require(!lockstatus);
-        require( _to != 0x0);
-        require(balances[msg.sender] >= _amount && _amount >= 0);
-        balances[msg.sender] = (balances[msg.sender]).sub(_amount);
-        balances[_to] = (balances[_to]).add(_amount);
-        emit Transfer(msg.sender, _to, _amount);
-        return true;
-    }
-
-    function burn(uint256 value) public onlyOwner returns (bool success) {
-        uint256 _value = value.mul(10 ** 18);
-        require(balances[msg.sender] >= _value);   
+    function burn(uint256 _value) public onlyOwner returns (bool) {
+        require(balances[msg.sender] >= _value, "Balance does not have enough tokens");   
         balances[msg.sender] = (balances[msg.sender]).sub(_value);            
         _totalsupply = _totalsupply.sub(_value);                     
         emit Burn(msg.sender, _value);
@@ -151,91 +204,68 @@ contract WGP is ERC20 {
     }
 
     function stopTransferToken() external onlyOwner onlyFinishedICO {
-        require(!lockstatus);
-        lockstatus = true;
+        _lockToken = true;
+        emit ChangeTokenLockStatus(true);
     }
 
     function startTransferToken() external onlyOwner onlyFinishedICO {
-        require(lockstatus);
-        lockstatus = false;
+        _lockToken = false;
+        emit ChangeTokenLockStatus(false);
     }
 
-    function manualMint(address receiver, uint256 _value) public onlyOwner returns (bool){
-        uint256 value = _value.mul(10 ** 18);
-        require(receiver != 0x0 && _value > 0);
+    function () external payable onlyICO onlyAllowICO {
+        
+    }
 
+    function manualMint(address receiver, uint256 _value) public onlyOwner{
+        uint256 value = _value.mul(10 ** 18);
+        mint(_owner, receiver, value);
+    }
+
+    function mint(address from, address receiver, uint256 value) internal {
+        require(receiver != address(0), "Address can not be 0x0");
+        require(value > 0, "Value should larger than 0");
         balances[receiver] = balances[receiver].add(value);
         _totalsupply = _totalsupply.add(value);
-        mintedTokens = mintedTokens.add(value);
-
-        require(_totalsupply <= maxCap);
-        emit Mint(owner, receiver, value);
-        emit Transfer(0, receiver, value);
+        require(_totalsupply <= _maxCap, "CrowdSale hit max cap");
+        emit Mint(from, receiver, value);
+        emit Transfer(address(0), receiver, value);
     }
     
-    function haltCrowdSale() external onlyOwner onlyICO {
-        require(!stopped);
-        stopped = true;
+    function haltCrowdSale() external onlyOwner {
+        _allowICO = false;
+        emit ChangeAllowICOStatus(false);
     }
 
-    function resumeCrowdSale() external onlyOwner onlyICO {
-        require(stopped);
-        stopped = false;
-    }
-    
-    function changeReceiveWallet(address newAddress) external onlyOwner {
-        require(newAddress != 0x0);
-        ethFundMain = newAddress;
+    function resumeCrowdSale() external onlyOwner {
+        _allowICO = true;
+        emit ChangeAllowICOStatus(true);
     }
 
-	function assignOwnership(address newOwner) public onlyOwner {
-	    require(newOwner != 0x0);
-	    balances[newOwner] = (balances[newOwner]).add(balances[owner]);
-	    balances[owner] = 0;
-	    owner = newOwner;
-	    emit Transfer(msg.sender, newOwner, balances[newOwner]);
+    function changeReceiveWallet(address payable newAddress) external onlyOwner {
+        require(newAddress != address(0), "Address can not be 0x0");
+        _ethFundMain = newAddress;
+        emit ChangeReceiveWallet(newAddress);
+    }
+
+	function assignOwnership(address newOwner) external onlyOwner {
+	    require(newOwner != address(0), "Address can not be 0x0");
+	    _owner = newOwner;
+	    emit ChangeOwnerShip(newOwner);
 	}
 
-    function forwardFunds() external onlyOwner { 
-        address myAddress = this;
-        ethFundMain.transfer(myAddress.balance);
-    }
-}
-
-contract WgpHolder {
-    WGP public wgp;
-    address public owner;
-    address public recipient;
-    uint256 public releaseDate;
-
-    modifier onlyOwner() {
-        require(msg.sender == owner);
-        _;
+    function forwardFunds() external onlyOwner {
+        require(_ethFundMain != address(0));
+        _ethFundMain.transfer(address(this).balance);
     }
 
-    constructor() public {
-        wgp = WGP(0xf9918ce795c6CDEA4875a906512BbC15a7d61Abd);
-        owner = msg.sender;
+    function haltTokenTransferFromAddress(address investor) external onlyOwner {
+        locked[investor] = true;
+        emit ChangeLockStatusFrom(investor, true);
     }
 
-    function setReleaseDate(uint256 newReleaseDate) external onlyOwner {
-        require(newReleaseDate > now);
-        releaseDate = newReleaseDate;
-    }
-
-    function setWgpRecipient(address newRecipient) external onlyOwner {
-        require(newRecipient != 0x0);
-        recipient = newRecipient;
-    }
-
-    function releaseWgp() external onlyOwner {
-        require(recipient != 0x0 && releaseDate != 0 && now > releaseDate);
-        uint256 balance = wgp.balanceOf(address(this));
-        wgp.transfer(recipient, balance);
-    }
-    
-    function changeOwnerShip(address newOwner) external onlyOwner {
-        require(newOwner != 0x0);
-        owner = newOwner;
+    function resumeTokenTransferFromAddress(address investor) external onlyOwner {
+        locked[investor] = false;
+        emit ChangeLockStatusFrom(investor, false);
     }
 }
